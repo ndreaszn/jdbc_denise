3c9bd56b8ad6c38f1c87033294a714de230190b5
Denise Andrea Name Ramirez <dnameramirez@gmail.com>
2026-06-01 10:02:39 +0200

    adapted Producto.java for db

--- database/src/main/java/org/example/Producto.java
+++ database/src/main/java/org/example/Producto.java
@@ -2,101 +2,72 @@ package org.example;
 
 public class Producto {
 
-    private int id;
+// adapted para el .sql
+    private int idProducto;
     private String nombre;
     private String descripcion;
     private int idSeccion;
     private int idMarca;
     private double precio;
     private int stock;
+    private String unidadMedida;
     private String codigoBarras;
+    private boolean activo;
+    private String nombreSeccion;
+    private String nombreMarca;
 
-    public Producto(String nombre, String descripcion, int idSeccion, int idMarca, double precio, int stock, String codigoBarras) {
+    public Producto() {}
+
+    public Producto(String nombre, String descripcion, int idSeccion, int idMarca, double precio, int stock, String unidadMedida, String codigoBarras) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.idSeccion = idSeccion;
         this.idMarca = idMarca;
         this.precio = precio;
         this.stock = stock;
+        this.unidadMedida = unidadMedida;
         this.codigoBarras = codigoBarras;
+        this.activo = true;
     }
 
-    public String getCodigoBarras() {
-        return codigoBarras;
-    }
+    public int getIdProducto() { return idProducto; }
+    public void setIdProducto(int v) { this.idProducto = v; }
 
-    public void setCodigoBarras(String codigoBarras) {
-        this.codigoBarras = codigoBarras;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String v) { this.nombre = v; }
 
-    public int getStock() {
-        return stock;
-    }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String v) { this.descripcion = v; }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+    public int getIdSeccion() { return idSeccion; }
+    public void setIdSeccion(int v) { this.idSeccion = v; }
 
-    public double getPrecio() {
-        return precio;
-    }
+    public int getIdMarca() { return idMarca; }
+    public void setIdMarca(int v) { this.idMarca = v; }
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
+    public double getPrecio() { return precio; }
+    public void setPrecio(double v) { this.precio = v; }
 
-    public int getIdMarca() {
-        return idMarca;
-    }
+    public int getStock() { return stock; }
+    public void setStock(int v) { this.stock = v; }
 
-    public void setIdMarca(int idMarca) {
-        this.idMarca = idMarca;
-    }
+    public String getUnidadMedida() { return unidadMedida; }
+    public void setUnidadMedida(String v) { this.unidadMedida = v; }
 
-    public int getIdSeccion() {
-        return idSeccion;
-    }
+    public String getCodigoBarras() { return codigoBarras; }
+    public void setCodigoBarras(String v) { this.codigoBarras = v; }
 
-    public void setIdSeccion(int idSeccion) {
-        this.idSeccion = idSeccion;
-    }
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean v) { this.activo = v; }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+    public String getNombreSeccion() { return nombreSeccion; }
+    public void setNombreSeccion(String v) { this.nombreSeccion = v; }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getNombreMarca() { return nombreMarca; }
+    public void setNombreMarca(String v) { this.nombreMarca = v; }
 
     @Override
     public String toString() {
-        return "Producto{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                ", idSeccion=" + idSeccion +
-                ", idMarca=" + idMarca +
-                ", precio=" + precio +
-                ", stock=" + stock +
-                ", codigoBarras='" + codigoBarras + '\'' +
-                '}';
+        return idProducto + " | " + nombre + " | " + precio + " € | " + stock + " uds | " + unidadMedida;
     }
 }
-
